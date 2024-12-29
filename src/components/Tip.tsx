e7f6ff1c95fdbed351e120d9b1dbca75cb96a8e0
manoj singh dashauni <dashauni007@gmail.com>
2024-12-29 16:24:02 +0530

    responsive

--- src/components/Tip.tsx
+++ src/components/Tip.tsx
@@ -1,17 +1,17 @@
 const Tip = () => {
   return (
-    <div className="absolute flex gap-5 bg-colordark h-auto py-4 pl-4 pr-12 w-auto rounded-full bottom-10 -right-[415px] hover:-right-10 transition-all">
+    <div className="absolute flex justify-center gap-5 bg-colordark h-auto py-4 pl-4 pr-12 w-auto rounded-full bottom-10 sm:-right-[415px] -right-[265px]  sm:hover:-right-10 hover:-right-16  transition-all">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
         fill="white"
-        className="sm:size-6 size-10"
+        className="sm:size-6 size-10 self-center"
       >
         <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
         <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
       </svg>
 
-      <p> Tip: You can drag, close, open, click, and explore.</p>
+      <p className="sm:w-full w-52"> Tip: You can drag, close, open, click, and explore.</p>
     </div>
   );
 };
