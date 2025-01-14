b36a1e4fac3422629daa5feb83b2973c4afbf645
manoj singh dashauni <dashauni007@gmail.com>
2025-01-14 12:02:33 +0530

    new project

--- src/data/ProjectList.tsx
+++ src/data/ProjectList.tsx
@@ -1,49 +1,57 @@
 import { Gemini,blog,country,desert,ecom,note,tictactoe } from "../assets/projects/Index"
 
+function id(){
+    return Math.ceil(Math.random() *1000)
+}
+
 export const ProjectList = [
     {
-        id:1,
+        id:id(),
+        projectTitle:"Product list - ecom",
+        projectSrc : desert,
+        projectLink : "https://framer-motion-award-o1eu.vercel.app/"
+    },
+    {
+        id:id(),
         projectTitle:"Recipe List Generator with gemini",
         projectSrc : Gemini,
         projectLink : "https://30-recipe-list-with-gemini.vercel.app/"
     },
     {
-        id:2,
+        id:id(),
+        projectTitle:"Product list - ecom",
+        projectSrc : desert,
+        projectLink : "https://20-product-list.vercel.app/"
+    },
+    {
+        id:id(),
         projectTitle:"Country api",
         projectSrc : country,
         projectLink : "https://22-country-api-rjtz.vercel.app/"
     },
     {
-        id:3,
+        id:id(),
         projectTitle:"CMS headless Ecommerce website",
         projectSrc : ecom,
         projectLink : "https://shop-store-nu.vercel.app/"
     },
     {
-        id:4,
+        id:id(),
         projectTitle:"Sanity based Blog upload website",
         projectSrc : blog,
         projectLink : "https://blackman-gray.vercel.app/"
     },
     {
-        id:5,
+        id:id(),
         projectTitle:"Daily Planner with authentication",
         projectSrc : note,
         projectLink : "https://29-todo.vercel.app/"
     },
     {
-        id:6,
+        id:id(),
         projectTitle:"Tic-Tac-Toe",
         projectSrc : tictactoe,
         projectLink : "https://02-typescript-tic-tac-toe.vercel.app/"
     },
-
-    {
-        id:6,
-        projectTitle:"Product list - ecom",
-        projectSrc : desert,
-        projectLink : "https://20-product-list.vercel.app/"
-    },
-
     
 ]
